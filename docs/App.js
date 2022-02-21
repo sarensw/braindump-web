edd5ac34822fbb739c636a05076bf03a1f7ad2d8
Stephan Arenswald <stephan@sarensw.com>
2022-02-21 03:59:14 +0100

    fix: build missing

--- docs/App.js
+++ docs/App.js
@@ -20,18 +20,20 @@ const App = () => {
   }, "Write down thoughts. Fast."), /* @__PURE__ */ React.createElement("div", {
     className: "mt-6 max-w-xl mx-auto text-center text-lg"
   }, "Offline notepad to quickly note down thoughts, meeting minutes, tasks and more. For developers. For techies. For keyboard lovers. Uses the Visual Studio Code editor underneath for a user experience that you are already used to."), /* @__PURE__ */ React.createElement("div", {
-    className: "mt-8 mx-auto flex flex-row gap-4 justify-center flex-wrap"
+    className: "text-center mt-8 mb-1 text-gray-500"
+  }, "v0.6.1"), /* @__PURE__ */ React.createElement("div", {
+    className: "mx-auto flex flex-row gap-4 justify-center flex-wrap"
   }, /* @__PURE__ */ React.createElement(DownloadButton, {
     platform: "win",
-    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.0_103/Braindump+Setup+0.6.0.exe"
+    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.1_105/Braindump+Setup+0.6.1.exe"
   }), /* @__PURE__ */ React.createElement(DownloadButton, {
     platform: "darwin",
-    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.0_103/Braindump-0.6.0.dmg"
+    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.1_105/Braindump-0.6.1.dmg"
   }), /* @__PURE__ */ React.createElement(DownloadButton, {
     platform: "linux",
-    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.0_103/braindump-0.6.0.tar.gz",
-    href2: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.0_103/braindump_0.6.0_amd64.deb",
-    href3: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.0_103/braindump-0.6.0.x86_64.rpm"
+    href: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.1_105/braindump-0.6.1.tar.gz",
+    href2: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.1_105/braindump_0.6.1_amd64.deb",
+    href3: "https://braindump-releases.s3.eu-central-1.amazonaws.com/Braindump_0.6.1_105/braindump-0.6.1.x86_64.rpm"
   })), /* @__PURE__ */ React.createElement("div", {
     className: "mt-6 mb-10 max-w-3xl mx-auto"
   }, /* @__PURE__ */ React.createElement("img", {
